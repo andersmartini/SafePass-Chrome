eeec54cc7e8b9934646029e998635772761cd351
Anders Martini <andersmartini2@gmail.com>
2016-03-04 16:04:40 +0100

    Promises and code-Cleanup!

--- background.js
+++ background.js
@@ -1,22 +1,57 @@
 "use strict"
 
-chrome.extension.onRequest.addListener(function(req, sender, res){
-    switch (req.method) {
+chrome.extension.onRequest.addListener(function(request, sender, respond){
+    switch (request.method) {
         case "getHash":
-            res(createHash(req.domain));
+            createHash(request)
+                .then(respond, respond);
             break;
-        case "signUp":
 
+        case "signUp":
+            createUser(request)
+                .then(initialize)
+                .then(createHash)
+                .then(respond,respond);
             break;
         case 'login':
-            
+            login(request)
+                .then(initialize)
+                .then(respond,respond);           
+            break;
+
         default:
+        respond({ok:false, err:"Invalid Method"});
         break;
     }
 })
 
+/*************************** Dummies ****************************/
+//  These funcions will be swapped for better ones when logged in!    
+//                   se [initializers] below! 
+
+//Actual implementation nested in initialize (see line 106)
+var createHash = function(request){
+    return new Promise(function(resolve, reject){
+        response = {ok:false, err:"Must Log In First!"}
+        reject(response);
+    })
+}
+
+//Actual implementation nested inside conneccSecrets (see line 134)
+var getSecrets = function(request){
+    return new Promise(function(resolve, reject){
+        response = {ok:false, err:"Must Log In First!"}
+        reject(response);
+    })
+}
+
+
+
+
 /*************************  UTILS  ***********************************/
+
 //extract interesting part of url, the part before '.com', '.org' etc..
+// mail.google.com returns "google", while login.portal.office.microsoft.com returns "microsoft"
 function parseHost(url){
     var host = tldjs.getDomain(url);
     var psfx = tldjs.getPublicSuffix(url);
@@ -35,113 +70,11 @@ function rehash(times, obj){
     return rehash(times-1, newObj);
 }
 
-
-
-
-
-
-
-
-/***************************Dummies****************************/
-
-//dummy implementation, needs to be initialized with intialize() !
-var createHash = function(){
-    return {ok:false, err:"Not initialized!"};
-}
-
-//dummy implementation needs to be initialized with connectSecrets()! 
-var getSecrets = function(){
-    return "not yet initialized!"
-}
-
-
-
-
-
-
-/*********************************INITIALIZERS************************************/
-
-
-//sets createHash to a function accepting a domain, mixing that domain with the password passed into initialize()
-
-function initialize(localPass, serverPass){
-
-    //enforce uniqueness and length on localPass, and login to server.
-    if( (localPass.length<6 && login(serverPass)) || localPass===serverPass){
-        return{ok:false, err:"couldn't log in, verify your passwords"}
-    }
-
-    createHash = function (domain){
-        if (localPass.length >1){
-            var domain = parseHost(domain);
-            //Create and update a hashobject
-            var shaObj = new jsSHA('SHA-512', "TEXT");
-            shaObj.update(domain);
-            shaObj.update(localPass);
-            //Shorten the output in case someone has microsoft accounts (they allow max 16chars)
-            var final =rehash(500, shaObj).substring(0,15);
-            return{ok:true, hash:final};
-        } else {
-            return{ok:false, err:"Pass not set!"};
-        }
-    }
-}
-
-
-
-/*  
-*   login towards server, 
-*   this basicly just checks that [serverPass] and [username] is correct
-*   credentialls will still have to be sent with each request.
-*
-*   consider adding some cookie-like mechanism unless extension-specific cookies are a thing in chrome
-*   to avoid this unnesscary passing of credentials - its Risky!
-*/
-function login(user, pass, res){
-
-    var xhr =new XMLHttpRequest();
-    xhr.open('POST', 'http://pass-safe.herokuapp.com/login', true);
-    xhr.withCredentials = true;
-    xhr.body.user = user;
-    xhr.body.password = pass;
-    xhr.onreadystatechange = function(response){
-        if(response.ok){
-            connectSecrets(user, pass);
-            return true;
-        }
-        return false;
-    }
-}
-
-
-
-
-//initializes adapter getSecrets with SafePass server credentials
-function connectSecrets(user, pass){
-    //getSecrets promises a Secret for the given domain. 
-    getSecrets = function(domain){
-        return new Promise(function(resolve, reject){
-            const xhr = new XMLHttpRequest();
-            xhr.open('POST', `/http://pass-safe.herokuapp.com/Secret/${domain}`, true);
-            xhr.withCredentials=true;
-            xhr.body.user = user;
-            xhr.body.pass = pass;
-            xhr.onreadystatechange = function(response){
-                response = JSON.parse(response);
-                if(response.ok){
-                    resolve(response.Secret);
-                }
-            }
-        })
-    }
-}
-
-
 //ensure compatability at all sites.
 //lower actual security of generated passwords in case they dont fullfill "security requirements" on target sites
 function ensureSymbols(pass){
     /*
-    to Be implemented
+    to Be implemented/7
     check final cropped password to make sure it has at least 1
         - lowercase letter
         - UpperCase Letter
@@ -152,3 +85,116 @@ function ensureSymbols(pass){
 
     */
 }
+
+
+
+
+/*********************************INITIALIZERS************************************/
+
+
+//sets createHash to a function accepting a domain, mixing that domain with the password passed into initialize()
+
+function initialize(request){
+    return new Promise(function(resolve, reject){
+
+        //enforce uniqueness and length on request.localPass, and login to server.
+        if( (request.localPass.length<6 && login(request.serverPass)) || request.localPass===request.serverPass){
+            request.err = "couldn't log in, verify your passwords"
+            return reject(request);
+        }
+        connectSecrets(request);
+        createHash = function (request){
+            return new Promise(function(resolve, reject){
+                var domain = parseHost(request.domain);
+                //Create and update a hashobject
+                var shaObj = new jsSHA('SHA-512', "TEXT");
+                shaObj.update(request.domain);
+                shaObj.update(request.Secret)
+                shaObj.update(localPass);
+                //Shorten the output in case someone has microsoft accounts (they allow max 16chars)
+                var final =rehash(500, shaObj).substring(0,15);
+                const response = {ok:true, hash:final};
+                return resolve(response);
+                
+            })
+        }
+    })
+}
+
+
+
+
+
+//initializes adapter getSecrets with SafePass server credentials
+function connectSecrets(request){
+    const serverPass = request.serverPass;
+    const userName = request.username
+    //getSecrets promises a Secret for the given domain. 
+    getSecrets = function(request){
+        return new Promise(function(resolve, reject){
+            const domain = parseHost(request.domain)
+            const xhr = new XMLHttprequestuest();
+            xhr.open('POST', '/http://pass-safe.herokuapp.com/Secret/'+domain, true);
+            xhr.withCredentials=true;
+            xhr.body.user = username;
+            xhr.body.pass = serverPass;
+            xhr.onreadystatechange = function(response){
+                response = JSON.parse(response);
+                if(response.ok){
+                    request.secret = response.Secret;
+                    return resolve(request);
+                }
+                reject(request);
+            }
+        })
+    }
+}
+
+
+
+/******************************* Remote Operations *************************************/
+
+function createUser(request){
+    return new Promise(function(resolve,reject){
+        const xhr = new XMLHttprequestuest();
+        xhr.open('POST','/http://pass-safe.herokuapp.com/signup');
+        xhr.withCredentials=true;
+        xhr.body.username = request.username;
+        xhr.body.password = request.serverPass;
+        xhr.onreadystatechange = function(response) {
+            response = JSON.parse(response);
+            if(response.ok){
+                return resolve(request);
+            }
+            request.err = response.err
+            reject(request);
+        }
+
+    })
+}
+
+/*  *******************************************************************************
+*   login towards server, 
+*   this basicly just checks that [serverPass] and [username] is correct
+*   credentialls will still have to be sent with each request.
+*
+*   consider adding some cookie-like mechanism unless extension-specific cookies are a thing in chrome
+*   to avoid this unnesscary passing of credentials - its Risky!  */
+
+function login(request){
+    return new Promise(function(resolve, reject){    
+        var xhr =new XMLHttpRequest();
+        xhr.open('POST', 'http://pass-safe.herokuapp.com/login', true);
+        xhr.withCredentials = true;
+        xhr.body.user = request.username;
+        xhr.body.password = request.serverPass; 
+        xhr.onreadystatechange = function(response){
+            if(response.ok){
+                connectSecrets(request);
+                return resolve(request);
+            }
+            return reject(request);
+    }
+    })
+}
+
--- inject.js
+++ inject.js
@@ -1,16 +1,7 @@
 var hotkey =60; //default hotkey '>'
 var hotkey2 = 62 // default alternative hotkey, for changing passwords '<'
 $(function(){
-    $(document).on("focus", "input[type=password]",function(e){
-        chrome.extension.sendRequest({method:"getHotkey"},function(res){
-            if(typeof(res.hotkey)=='number'){
-                hotkey = res.hotkey;
-            }
-            if(typeof(res.hotkey2)=='number'){
-                hotkey2= res.hotkey2;
-            }
-        })
-    })
+
     $(document).on("keypress","input[type=password]", function(event){
         if(event.keyCode==hotkey ){
             var that = $(this);
@@ -24,4 +15,26 @@ $(function(){
 
         }
     })
+
+ 
+
+
+
+
+/* Functionality to use custom hotkey, currently not working so taking out of implementation for now
+
+
+    $(document).on("focus", "input[type=password]",function(e){
+        chrome.extension.sendRequest({method:"getHotkey"},function(res){
+            if(typeof(res.hotkey)=='number'){
+                hotkey = res.hotkey;
+            }
+            if(typeof(res.hotkey2)=='number'){
+                hotkey2= res.hotkey2;
+            }
+        })
+    })
+
+*/
+
 })
